87aa7ccdc59f6764202fdeccc39d5aae6ce68ef6
mymx2 <32814920+mymx2@users.noreply.github.com>
2026-02-09 22:42:18 +0800

    chore: sync

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 import io.github.mymx2.plugin.dyIncludeProjects
 
 plugins {
-  id("io.github.mymx2.build") version "1.3.8"
-  id("io.github.mymx2.build.feature.catalogs") version "1.3.8"
-  id("io.github.mymx2.plugin.dy.example.settings") version "1.3.8"
+  id("io.github.mymx2.build") version "1.3.9"
+  id("io.github.mymx2.build.feature.catalogs") version "1.3.9"
+  id("io.github.mymx2.plugin.dy.example.settings") version "1.3.9"
   id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
