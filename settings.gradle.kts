a7a56d85de4e79fc2e9e54715d6562b02f49784b
mymx2 <32814920+mymx2@users.noreply.github.com>
2026-07-31 19:51:04 +0800

    chore: upgrade

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,9 @@
 import io.github.mymx2.plugin.dyIncludeProjects
 
 plugins {
-  id("io.github.mymx2.build") version "1.5.4"
-  id("io.github.mymx2.build.feature.catalogs") version "1.5.4"
-  id("io.github.mymx2.plugin.dy.example.settings") version "1.5.4"
+  id("io.github.mymx2.build") version "1.5.5"
+  id("io.github.mymx2.build.feature.catalogs") version "1.5.5"
+  id("io.github.mymx2.plugin.dy.example.settings") version "1.5.5"
   id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
